48312981582112f8566c952c312be205c4acc526
Stefan Seifert <nine@detonation.org>
2013-07-19 08:12:35 +0200

    Implement password repeat check in JavaScript

--- root/static/js/scripts.js
+++ root/static/js/scripts.js
@@ -8,3 +8,25 @@ function paste(link, after) {
     location.href = link.href + ';' + id + ';after=' + after;
     return false;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var edit_object_form = document.getElementById('edit_object_form');
+    if (edit_object_form) {
+        edit_object_form.onsubmit = function() {
+            var password = edit_object_form.querySelector('label.password input');
+            var repeat   = edit_object_form.querySelector('label.password_repeat input');
+            if (password && repeat) {
+                var repeat_error = repeat.parentNode.querySelector('.repeat_error');
+                if (password.value == repeat.value) {
+                    repeat_error.style.display = '';
+                    return true;
+                }
+                else {
+                    repeat_error.style.display = 'block';
+                    alert(false);
+                    return false;
+                }
+            }
+        }
+    }
+}, false);
