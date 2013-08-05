5b101578344fd6fb767fc6e5f3eff0bae448d34d
Stefan Seifert <nine@detonation.org>
2013-08-05 06:31:28 +0200

    Treat empty string as undef in set_value of Integer attributes

--- lib/CiderCMS/Attribute/Integer.pm
+++ lib/CiderCMS/Attribute/Integer.pm
@@ -27,6 +27,19 @@ sub db_type {
     return 'integer';
 }
 
+=head2 set_data($data)
+
+Sets this attributes data to the given value.
+
+=cut
+
+sub set_data {
+    my ($self, $data) = @_;
+
+    return $self->{data} = $data if defined $data and $data ne '';
+    return $self->{data} = undef;
+}
+
 =head1 AUTHOR
 
 Stefan Seifert
