87f5754cad549eb69fd70678417eda198b3d617e
Stefan Seifert <nine@detonation.org>
2013-10-31 21:34:08 +0100

    Rules in registration email

--- lib/CiderCMS/Controller/Custom/Registration.pm
+++ lib/CiderCMS/Controller/Custom/Registration.pm
@@ -43,8 +43,44 @@ sub register : CiderCMS('register') {
             ->to($params->{email})
             ->subject('Bestätigung der Anmeldung zu ' . $c->stash->{instance})
             ->text_body(
-                "Bitte klicken Sie auf den folgenden Link um die Anmeldung zu bestätigen:\n"
+                "Durch Klick auf folgenden Link bestätigen Sie die Anmeldung und, dass Sie
+untenstehende Regeln akzeptieren:\n\n"
                 . $verify_uri
+                . <<RULES
+
+
+Regeln
+
+Eintragungen sind bis maximal 12 Stunden vor dem jeweiligen Termin möglich.
+
+Minimum der Reservierungsdauer sind 30 Minuten.
+
+Tank- und Pflegezeiten unmittelbar nach dem Flug sind in die Reservierung mit
+einzurechnen.
+
+Reservierungen, bei denen das Flugzeug über Nacht nicht im Heimathangar steht,
+sind mit dem Linzer Vorstand abzustimmen.
+
+Wenn das Flugzeug nicht innerhalb der ersten 15 Minuten des Reservierungs-
+zeitraumes durch den Reservierenden in Anspruch genommen wird, verfällt die
+Reservierung und das Flugzeug steht im betreffenden Zeitraum frei zur Verfügung.
+Zeitliche Überziehungen (zu spätes Zurückkommen) gehen zu Lasten der nächsten
+Reservierung (sollte unmittelbar eine anschließen).
+
+Nicht „reservierte“ Flugzeiten können so wie bisher in Anspruch genommen werden.
+
+Der Pilot muss sich vor Flugantritt davon überzeugen, dass für den von ihm
+geplanten Zeitraum keine Reservierung vorliegt. Das Flugzeug muss aber
+spätestens bei Beginn des nächsten Reservierungszeitraumes bereitstehen
+(Tankzeiten unmittelbar nach dem Flug sind mit einzurechnen).
+
+Reservierungen, die nicht in Anspruch genommen werden können, müssen vom
+Reservierenden so lange als möglich im Vorhinein wieder storniert werden.
+Steht ein Flugzeug aus z.B. Wartungsgründen nicht zur Verfügung, so ist dies
+in der Reservierungsliste mit „UNKLAR“ gekennzeichnet. Bei bereits vorliegenden
+Reservierungen werden die Betroffenen per Mail (Adresse bei der Registrierung
+angegeben) verständigt.
+RULES
             )
             ->send;
 
